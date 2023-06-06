25dbba249f3cab60e530ebb0e351320f07cf56d8
LeaderbotX400 <eric@singerfamily.ca>
2023-06-06 12:17:46 -0600

    add formatting

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import './assets/main.css'
+import "primevue/resources/primevue.min.css";
+import "primevue/resources/themes/tailwind-light/theme.css"
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Plugins
+import PrimeVue from 'primevue/config'
+import { createPinia } from 'pinia'
+
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(router)
+app.use(pinia)
+app.use(PrimeVue)
 
 app.mount('#app')

c853959b451a7e3da44bcf64338621b68a38e892
LeaderbotX400 <eric@singerfamily.ca>
2023-06-06 11:48:49 -0600

    init

---
+++ vite.config.ts
@@ -0,0 +1,50 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// Plugins
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import VueRouter from 'unplugin-vue-router/vite'
+import { VueRouterAutoImports } from "unplugin-vue-router";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VueRouter({
+      dts: true,
+    }),
+    vue(),
+    Components({
+      dts: true,
+    }),
+    AutoImport({
+      dts: true,
+      eslintrc: {
+        enabled: true,
+      },
+      imports: [
+        'vue',
+        VueRouterAutoImports,
+        '@vueuse/core',
+        'pinia'
+      ]
+    })
+  ],
+  define: {
+    // vue
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_DEVTOOLS__: false,
+  },
+  esbuild: {
+    drop: ["console", "debugger"],
+    legalComments: "none",
+    format: "esm",
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
